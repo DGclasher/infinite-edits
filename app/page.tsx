dcb3067f347278b388662fe2957cf1bafda39c64
Akash Dey <99585944+Ne0sky@users.noreply.github.com>
2024-04-26 19:12:03 +0530

    initial commit-tailwing config

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       hiiiii
     </div>
   );
