46c08c825e6f5c4254700b0600acd09ba9d4586d
Akash Dey <99585944+Ne0sky@users.noreply.github.com>
2024-04-28 09:48:57 +0530

    style scrollbar

--- components/price-section.tsx
+++ components/price-section.tsx
@@ -29,8 +29,8 @@ const PricingSection = () => {
         </div>
 
         <div className="bg-gradient-to-b h-[650px] py-8 from-neutral-800 to-black backdrop-blur-md border border-neutral-600 rounded-xl w-[350px] shadow-md p-6">
-          <h2 className="text-2xl  text-white font-semibold mb-1">Video Edit</h2>
-          <p className="text-neutral-300 mb-8">This package includes comprehensive video editing services.</p>
+          <h2 className="text-2xl  text-white font-semibold mb-1">Reels video edit</h2>
+          <p className="text-neutral-300 mb-8"> Includes comprehensive video editing services.</p>
           <p className="text-5xl font-bold text-gray-200">₹ 3000</p>
           <button className="w-full justify-center text-center  bg-indigo-700 px-6 py-3 rounded-lg text-white flex items-center gap-2 mt-4">Get Started </button>
           <ul className="mt-4 text-start text-gray-300 text space-y-4">
@@ -42,9 +42,9 @@ const PricingSection = () => {
         </div>
 
         <div className="bg-gradient-to-b h-[650px] py-8 from-neutral-800 to-black backdrop-blur-md border border-neutral-600 rounded-xl w-[350px] shadow-md p-6">
-          <h2 className="text-2xl  text-white font-semibold mb-1">Custom</h2>
+          <h2 className="text-2xl  text-white font-semibold mb-1">Long video edits</h2>
           <p className="text-neutral-300 mb-8">Get a custom quote for your specific video editing needs.</p>
-          <p className="text-4xl font-bold text-gray-200">₹ 5000</p>
+          <p className="text-5xl font-bold text-gray-200">₹ 5000</p>
           <button className="w-full justify-center text-center  bg-indigo-700 px-6 py-3 rounded-lg text-white flex items-center gap-2 mt-4">Get Started </button>
           <ul className="mt-4 text-start text-gray-300  space-y-4">
             <li className="flex items-center gap-2 border-b py-4"><FaCheckCircle className="text-green-500"/>Customized video editing solutions</li>
