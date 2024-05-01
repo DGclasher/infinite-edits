632330bb213cd9aeb1c2d37daa62c10f46f8a926
Akash Dey <99585944+Ne0sky@users.noreply.github.com>
2024-05-01 21:04:28 +0530

    update style of graphic design page

--- app/graphicdesign/page.tsx
+++ app/graphicdesign/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { BackgroundBeams } from "@/components/ui/background-beams";
 import Image from "next/image";
 import { useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
+import Link from "next/link";
 
 interface ImageData {
   src: string;
@@ -95,70 +98,74 @@ const GraphicDesign = () => {
     setFile(null);
   };
   return (
-    <div className="bg-black relative bg-grid-white/[0.08] overflow-x-hidden text-center">
-      <div className="absolute -top-80 z-10 flex left-0 items-center">
-        <div className="w-[500px] h-[1000px] -rotate-45  rounded-full z-2 blur-3xl bg-gradient-to-br from-indigo-600/50 to-transparent "></div>
-      </div>
-    <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-20 px-2">
-    <div className="relative mb-12">
-            <span className="relative text-white inline-block text-4xl">
-              Graphic Design
-              <Image
-                src={"/curve.png"}
-                className="absolute top-full left-0 w-full mt-1"
-                alt="Curve"
-                width={100}
-                height={100}
-              />
-            </span>
-          </div>
-      <p className="mt-4 text-lg font-normal text-neutral-300 max-w-lg text-center mx-auto px-4">
-        Explore our graphic design works.
-      </p>
-    </div>
-
-    <div className="w-full columns-1 z-10 relative md:columns-2 lg:columns-3 space-y-5 gap-5 p-5 ">
-      {imageData.map((data, index) => {
-        return (
-          <div
-            key={index}
-            onClick={() => openModal(data)}
-            className="overflow-hidden rounded-xl p-2"
-          >
+    <div className=" bg-gradient-to-b  from-neutral-950  max-w-screen  overflow-x-hidden text-center">
+      <BackgroundBeams />
+      <div className="p-4 mx-auto relative text-center z-10 flex flex-col items-center justify-center w-full pt-10 md:pt-20 px-2">
+        <div className="relative mb-12">
+          <span className="relative text-white inline-block text-4xl">
+            Graphic Design
             <Image
+              src={"/curve.png"}
+              className="absolute top-full left-0 w-full mt-1"
+              alt="Curve"
+              width={400}
+              height={400}
+            />
+          </span>
+        </div>
+        <p className="mt-4 text-lg font-normal text-neutral-300 max-w-lg text-center mx-auto px-4">
+          Explore our graphic design works.
+        </p>
+        <Link
+          href={"/contact"}
+          className="bg-gradient-to-r w-48 text-xl from-indigo-800/20 z-20 to bg-black px-6 py-3 rounded-full text-white flex items-center gap-2 mt-4 border border-indigo-500"
+        >
+          Contact us <FaArrowRight />
+        </Link>
+      </div>
+
+      <div className="w-full columns-1 z-10 relative md:columns-2 lg:columns-3 space-y-5 gap-5 p-5 ">
+        {imageData.map((data, index) => {
+          return (
+            <div
               key={index}
-              className="rounded-xl overflow-hidden object-cover w-full cursor-pointer h-auto shadow-lg"
-              src={data.src}
-              alt="Porfolio_img"
+              onClick={() => openModal(data)}
+              className="overflow-hidden rounded-xl p-2"
+            >
+              <Image
+                key={index}
+                className="rounded-xl overflow-hidden object-cover w-full cursor-pointer h-auto shadow-lg"
+                src={data.src}
+                alt="Porfolio_img"
+                width={500}
+                height={500}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div className="popup-media" style={{ display: file ? "block" : "none" }}>
+        <span onClick={() => setFile(null)}>&times;</span>
+
+        {file ? (
+          file.type === "video" ? (
+            <video src={file.src} autoPlay controls className="w-full h-full" />
+          ) : file.src ? (
+            <Image
+              src={file.src}
+              alt="Media content"
+              className="w-full h-full object-cover"
               width={500}
               height={500}
             />
-          </div>
-        );
-      })}
-    </div>
-    <div className="popup-media" style={{ display: file ? "block" : "none" }}>
-      <span onClick={() => setFile(null)}>&times;</span>
-
-      {file ? (
-        file.type === "video" ? (
-          <video src={file.src} autoPlay controls className="w-full h-full" />
-        ) : file.src ? (
-          <Image
-            src={file.src}
-            alt="Media content"
-            className="w-full h-full object-cover"
-            width={500}
-            height={500}
-          />
+          ) : (
+            <div className="text-center">No Image Available</div>
+          )
         ) : (
-          <div className="text-center">No Image Available</div>
-        )
-      ) : (
-        <div className="text-center">No Content to Display</div>
-      )}
+          <div className="text-center">No Content to Display</div>
+        )}
+      </div>
     </div>
-  </div>
   );
 };
 
